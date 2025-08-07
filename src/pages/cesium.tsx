8c94757ad0544cdd434a70ac6071e6fcbc283845
habibmuhammadthariq <thariqhabibmuhammad@gmail.com>
2025-08-07 23:07:19 +0700

    fix: building error

--- src/pages/cesium.tsx
+++ src/pages/cesium.tsx
@@ -1,20 +1,20 @@
-// // import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 // import Head from "next/head";
-// // import "../public/cesium/Widgets/widgets.css";
+// import "../public/cesium/Widgets/widgets.css";
 
-// // const Cesium = dynamic(
-// //   () => import('../components/cesium'),
-// //   { ssr: false}
-// // );
+// const Cesium = dynamic(
+//   () => import('../components/cesium'),
+//   { ssr: false}
+// );
 
-// export default function CesiumPage() {
-//   return (
-//     <>
-//       <Head>
-//         {/* The stylesheet is now imported directly */}
-//         <link rel="stylesheet" href="/cesium/Widgets/widgets.css" />
-//       </Head>
-//       {/* <Cesium /> */}
-//     </>
-//   )
-// }
+export default function CesiumPage() {
+  return (
+    <>
+      {/* <Head> */}
+        {/* The stylesheet is now imported directly */}
+        {/* <link rel="stylesheet" href="/cesium/Widgets/widgets.css" /> */}
+      {/* </Head> */}
+      {/* <Cesium /> */}
+    </>
+  )
+}
